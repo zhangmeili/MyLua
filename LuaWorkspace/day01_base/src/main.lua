3bafc100d8bacdb0d40178eab47416c5449b9d3f
zhangmeili <Aimee@zhangmeilideMacBook-Pro.local>
2016-10-27 22:17:59 +0800

    LUA语言语法，对象的声明

---
+++ LuaWorkspace/day01_base/src/main.lua
@@ -0,0 +1,50 @@
+-- autor zml
+-- date 2016/10/19
+--lua语言基本语法练习
+
+--打印输出内容
+--print("hello")
+
+--定义全局变了 num,输出num
+--num=100
+--print(num)
+
+--定义全局变量 name ，输出name
+--name="zml"
+--print(name)
+
+--定义方法 name，在方法中输出内容
+--function name()
+--	print("123")
+--
+--end
+--
+--name()
+
+--定义可传入参数的方法，方法中 使用if流程语句 输出相应内容
+--function name(a,b)
+--	if(a<b)
+--	then
+--		print("aaaaa")
+--	else
+--		print("bbbbbb")
+--	end
+--end
+
+--name(10,20)
+
+
+
+--for循环语句练习
+--for var=0, 100 do
+--	print(var)
+--
+--end
+
+
+--定义局部变量，执行while循环语句
+--local a=80
+--while (a<=100) do
+--	print(a)
+--	a=a+1
+--end
---
+++ LuaWorkspace/day02_obj/src/main.lua
@@ -0,0 +1,22 @@
+--autor zml
+--date 2016/10/20
+--lua对象的处理
+
+--申明一个对象 person
+--Person={}
+--给对象赋值(方法1，方法2)
+--Person.name="zml"
+--Person.age=20
+--Person["sex"]="woman"
+--
+--for key, var in pairs(Person) do
+--	print(key,var)
+--end
+
+
+--申明一个有初始值的对象 person
+--Person ={name="123",age=20,["sex"]="men"}
+--print(Person["name"])
+--print(Person.sex)
+
+
